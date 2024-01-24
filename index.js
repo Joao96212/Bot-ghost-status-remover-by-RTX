782f922f94832ca79c81c2dd0ac5b9d0824a7409
Joao96212 <140561326+Joao96212@users.noreply.github.com>
2024-01-24 14:17:16 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["HypeSt0re System","Fazendo grana!","Playing HypeSt0re"];
 
 
 let currentIndex = 0;
